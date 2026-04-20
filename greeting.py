4381f07c4aa465ec6ba8cf12f2b4304104a429e1
maienduo67 <philbert.siaguru@gmail.com>
2026-04-20 20:21:06 +1000

    Create greeting.py

---
+++ greeting.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+print("Hello,", name)
+print("Nice to meet you,", name)
+if name.lower() == "phil": 
+  print("Yo that's you!")
+else:
+  print("You're not Phil.")
